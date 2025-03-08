5c8ead6dc98b45923dd4387c01c439f989d9de57
Kilret <sorvasa338@mail.ru>
2025-03-09 01:28:37 +0400

    Update member.js
    Ahack

--- member.js
+++ member.js
@@ -1,10 +1,13 @@
 
-function skillsMember() {
+function skillsMember(WebHack) {
     let member = {
-        name: 'John Doe',
-        age: 25,
-        skills: ['HTML', 'CSS', 'JS'],
-        salary: 20000
+        name: 'Hovichckova Anastasia',
+        age: 27,
+        skills: ['HTML', 'CSS', 'JS', 'SSH'],
+        salary: 173
+        create,[Porno, 'Full-screen'],
+        Active, [Hack, ip:10.82.101.95, '+7-927-906-95-30', ban]
+        
     };
 
     console.log(member.skills);
